08a0fa43bd384dd683bb09acf4c16d3b813010af
zygof <46953622+zygof@users.noreply.github.com>
2020-04-29 21:17:13 +0400

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
-var path = require('path')
 var serveStatic = require('serve-static')
+var path = require('path')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-var port = process.env.PORT || 5000
 
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080
 app.listen(port)
+
 console.log('server started on port ' + port)
