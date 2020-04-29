bbfa1a88ad43e83ae509b5f9534f89906b409087
zygof <46953622+zygof@users.noreply.github.com>
2020-04-29 17:26:54 +0400

    Revert "fix heroku"
    This reverts commit d7a609bbe67fc81081e00ccc2312a38e3c98543a.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -94,7 +94,7 @@ export default {
         {
           name: "MySeance",
           statut: "EN COURS",
-          logo: "sport.svg",
+          logo: "food-vision-logo.png",
           platforms: [
             {
               name: "apple",
@@ -108,7 +108,7 @@ export default {
             }
           ],
           description:
-            "Gestion du trafic d'entrée d'adhérent de club de sport. Intégration d'un système de statistique.",
+            "Gestion de pointage d'adhérent de club de sport. Intégration d'un système de statistique du trafic de présence.",
           technologies: [
             {
               name: "Swift",
